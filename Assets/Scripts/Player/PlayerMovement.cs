bd629e8c233c570dbc06108127855235aeafed10
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-29 19:45:37 +0000

    Character Moving

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float runSpeed = 40f;
+
+    private CharacterController2D _controller;
+    private float _horizontalMove = 0f;
+    private bool _jump = false;
+    private bool _crouch = false;
+    
+    void Start()
+    {
+        _controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            _crouch = true;
+        }else if (Input.GetButtonUp("Crouch"))
+        {
+            _crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        _controller.Move(_horizontalMove * Time.fixedDeltaTime, _crouch, _jump);
+        Debug.Log(_horizontalMove);
+        _jump = false;
+    }
+}
