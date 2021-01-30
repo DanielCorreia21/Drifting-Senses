f84d8a393642e43e679ea360f700eba17c574802
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-30 12:46:44 +0000

    Boss and character in vision scene

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -11,20 +11,26 @@ public class EnemyController : MonoBehaviour
     private Transform _canvas;
     private HealthBarController _healthBar;
     private float maxHealth;
+    private Transform _player;
 
     void Start()
     {
         if (isBoss)
         {
             _canvas = GameObject.FindGameObjectWithTag("Canvas").transform;
-            _healthBar = Instantiate(healthBarPrefab, _canvas).GetComponent<HealthBarController>();
             maxHealth = health;
+            _player = GameObject.FindGameObjectWithTag("Character").transform;
+            _healthBar = null;
         }
     }
 
     void Update()
     {
-        
+        if(_healthBar == null && Vector3.Distance(_player.position, transform.position) < 5f)
+        {
+            _healthBar = Instantiate(healthBarPrefab, _canvas).GetComponent<HealthBarController>();
+            Debug.Log(Vector3.Distance(_player.position, transform.position));
+        }
     }
 
     public void TakeDamage(float damage)
