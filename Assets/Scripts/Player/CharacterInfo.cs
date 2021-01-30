188f1f1d9598b85012b3a1ec3cca472688794714
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-30 21:36:58 +0000

    Character regens health

--- Assets/Scripts/Player/CharacterInfo.cs
+++ Assets/Scripts/Player/CharacterInfo.cs
@@ -6,7 +6,9 @@ public class CharacterInfo : MonoBehaviour
 {
 
     public float health = 100f;
+    public float regenRate = 0.5f;
     public GameObject healthBarPrefab;
+    public bool regenActive = true;
 
     private float maxHealth;
     private HealthBarController _healthBar;
@@ -15,19 +17,36 @@ public class CharacterInfo : MonoBehaviour
     {
         maxHealth = health;
         _healthBar = Instantiate(healthBarPrefab, GameObject.FindGameObjectWithTag("Canvas").transform).GetComponent<HealthBarController>();
+        if (regenActive)
+        {
+            InvokeRepeating(nameof(RegenHealth), 0.2f, 0.2f);
+        }
     }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
-        float percentage = health / maxHealth;
-        percentage = percentage < 0 ? 0 : percentage;
-        _healthBar.UpdateHealthBar(percentage);
+        UpdateHealthBar();
 
         if(health <= 0)
         {
             StartCoroutine(LevelManager.Instance.ResetScene());
         }
         //TODO death
+    }
+
+    private void RegenHealth()
+    {
+        health += regenRate;
+        health = health > maxHealth ? maxHealth : health;
+
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        float percentage = health / maxHealth;
+        percentage = percentage < 0 ? 0 : percentage;
+        _healthBar.UpdateHealthBar(percentage);
     }
 }
