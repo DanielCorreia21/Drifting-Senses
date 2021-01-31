8a2ff6ddf918245fea04030b6153f8132730e22e
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-31 11:12:27 +0000

    Boss explosion attack

---
+++ Assets/Scripts/Enemy/Boss/Gluttony/ExplosionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        StartCoroutine(RemoveExplosion());
+    }
+
+    IEnumerator RemoveExplosion()
+    {
+        yield return new WaitForSecondsRealtime(0.75f);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Enemy/Boss/Gluttony/FoodExplosive.cs
+++ Assets/Scripts/Enemy/Boss/Gluttony/FoodExplosive.cs
@@ -7,6 +7,8 @@ public class FoodExplosive : MonoBehaviour
     public float speed = 20f;
     public float bulletDamage = 25f;
     public Rigidbody2D rb;
+    public GameObject explosionPrefab;
+    public float explosionRadius = 1f;
 
     void Start()
     {
@@ -20,24 +22,33 @@ public class FoodExplosive : MonoBehaviour
         //when we destroy, we make an explosion
         //We show explosion on screeen, and do a sphereCast to see if player is inside
         //Or check distance between this and the player, and check if less than blast radius
-        Destroy(gameObject);
+
+        //ignore bullets
+        if(collision.GetComponent<BulletController>() != null) { 
+            return;
+        }
+
+        GameObject explosion = Instantiate(explosionPrefab, LevelManager.Instance.transform);
+        explosion.transform.position = gameObject.transform.position;
+        Debug.Log("Explosion");
 
 
         if (collision != null)
         {
-            CharacterInfo character = collision.GetComponent<CharacterInfo>();
-            if (character != null)
+            GameObject characterObj = GameObject.FindGameObjectWithTag("Character");
+            if(Vector2.Distance(characterObj.transform.position, transform.position) < this.explosionRadius)
             {
-                character.TakeDamage(bulletDamage);
-
+                CharacterInfo characterInfo = characterObj.GetComponent<CharacterInfo>();
+                characterInfo.TakeDamage(bulletDamage);
             }
 
         }
+        Destroy(gameObject);
     }
 
     IEnumerator DestroyAfterTime()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(1.5f);
         if (gameObject != null)
         {
             Destroy(gameObject);
